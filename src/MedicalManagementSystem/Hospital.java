6d123f5dcff918299818e550dcfb92d0cfc80b73
Victor Olamide <akinnusivictor098@gmail.com>
2022-06-23 14:28:57 +0100

    newUpdate

--- src/MedicalManagementSystem/Hospital.java
+++ src/MedicalManagementSystem/Hospital.java
@@ -3,4 +3,5 @@ package MedicalManagementSystem;
 public class Hospital {
     private String hospitalName;
     private String hospitalAddress;
+    private Arraylist<Doctor> doctor = new Arralist<>;
 }
